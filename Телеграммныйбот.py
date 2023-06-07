dd2f81ce635e8d96cede5cc60826b3350e2c01c9
hacanov <123230509+hacanov@users.noreply.github.com>
2023-06-08 00:18:52 +0300

    Add files via upload

---
+++ Телеграммныйбот.py
@@ -0,0 +1,14 @@
+import telebot;
+bot = telebot.TeleBot('6230307816:AAFykI7EAk49yFK5oSJGtfMZZhAt7r6dLnI')
+
+bot.polling(none_stop=True, interval=0)
+@bot.message_handler(content_types=['text'])
+
+def get_text_messages(message):
+            if  message.text == "Привет":
+                bot.send_message(message.from_user.id, "Привет, чем я могу тебе помочь?")
+            elif message.text == "/help":
+                bot.send_message(message.from_user.id, "Напиши привет")
+            else:
+                bot.send_message(message.from_user.id, "Я тебя не понимаю. Напиши /help.")
+
